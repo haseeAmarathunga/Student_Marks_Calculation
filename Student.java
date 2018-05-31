c865916de905f12447157febdb76ad7f900496b1
Hasitha Amarathunga <haseeamarathunga@gmail.com>
2018-05-31 06:50:42 -0700

    Updated

--- Student.java
+++ Student.java
@@ -1,12 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 
 /**
  *
- * @author 2016cs023
+ * @author Hasee Amarathunga
  */
 public class Student {
     String Reg_No,gender,name,DOB;
